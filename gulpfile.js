9a89be2bacfed7c9e2181aa92707b34aab652847
Vijai Rajpal <vijai.p.rajpal@gmail.com>
2016-11-16 21:30:02 -0800

    app structure and sass processing via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload');
+    
 
 gulp.task('hello-world', function() { 
     console.log("Hello Worlds!");
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./app/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dest'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./app/styles/*.scss', ['sass']);
 });
